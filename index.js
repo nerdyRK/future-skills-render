57be8d53a36d1ebce31b8e357128c195e6bc3668
Ravk <93036264+nerdyRK@users.noreply.github.com>
2024-08-20 18:05:08 +0530

    minor change

--- index.js
+++ index.js
@@ -8,10 +8,16 @@ import path from "path";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
 
+const corsOptions = {
+  origin:"*",
+  optionsSuccessStatus: 200,
+};
+
+
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors(corsOptions));
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
